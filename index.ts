e1ec46de186e21fec897c63c99190500b7a5bc99
fzuin <fabiozuin@outlook.com>
2023-08-25 15:05:37 +0100

    Typescript Course Starter - Severus integration

---
+++ index.ts
@@ -0,0 +1,22 @@
+class YourFirstClass {
+  constructor() {
+    console.log("This is your First class!");
+    this.myFirstOutput();
+    this.myFirstSum();
+  }
+
+  /**
+   * @Severus 1.1.1 Typescript basic operations
+   * How can we display data on the console using typescript?
+   * Display the message hello-world
+   */
+  myFirstOutput() {}
+
+  /**
+   * @Severus 1.1.2 Typescript basic operations
+   * You should recieve 2 parameters here as numbers and log the sum of both using console.log
+   */
+  myFirstSum() {}
+}
+
+const test = new YourFirstClass();
